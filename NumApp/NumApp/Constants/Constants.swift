95c3cb13daaf1d872928ddd79244e1df40dd2ad4
Konstantin Kosolapov <kkosolapov@gmail.com>
2024-03-04 11:42:18 +0200

    Add files via upload

---
+++ NumApp/NumApp/Constants/Constants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Constants {
+    
+    static var max_number = Int.min;
+    static var min_number = Int.max;
+    static var median: Int = 0;
+    static var sum: Int = 0;
+    static var count: Int = 0;
+    static var average: Double = 0.0;
+}
