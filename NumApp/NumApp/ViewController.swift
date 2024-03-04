22cd32da536441a49d00d4efc7178aa261239999
Konstantin Kosolapov <kkosolapov@gmail.com>
2024-03-04 11:41:15 +0200

    Add files via upload

---
+++ NumApp/NumApp/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    var homeViewComtroller = HomeViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
